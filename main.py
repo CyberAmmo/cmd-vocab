1158c2b9d666810bc3eb3f334eaed762213175a9
Vlad <superman.back@ukr.net>
2021-05-17 22:32:29 +0300

    A new vocab app of mine

---
+++ main.py
@@ -0,0 +1,22 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open('data.json'))
+
+def vocab(w):
+    if word in data:
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        decition = input(f'Did you mean {get_close_matches(w, data.keys())[0]}? Enter y or n: ')
+        if decition == 'y':
+            return data[get_close_matches(w, data.keys())[0]]
+        elif decition == 'n':
+            return 'Word doesnt exist'
+        else:
+            return 'Unsuported command!'
+    else:
+        return 'No such word in'
+
+word = input('Enter a word here: ').lower()
+
+print(vocab(word))
